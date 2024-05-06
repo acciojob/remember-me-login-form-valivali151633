2d5f86d6ce7d507e3b1a7f59529b00c6e9ff1c44
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2024-05-06 21:35:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+window.onload = function() {
+    var usernameInput = document.getElementById("username");
+    var passwordInput = document.getElementById("password");
+    var rememberCheckbox = document.getElementById("checkbox");
+    var submitBtn = document.getElementById("submit");
+    var existingBtn = document.getElementById("existing");
+
+    submitBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        var username = usernameInput.value;
+        var password = passwordInput.value;
+
+        if (rememberCheckbox.checked) {
+            localStorage.setItem("username", username);
+            localStorage.setItem("password", password);
+        } else {
+            localStorage.removeItem("username");
+            localStorage.removeItem("password");
+        }
+
+        alert("Logged in as " + username);
+    });
+
+    if (localStorage.getItem("username") && localStorage.getItem("password")) {
+        existingBtn.style.display = "block";
+    }
+
+    existingBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        var username = localStorage.getItem("username");
+        alert("Logged in as " + username);
+    });
+};
